7cf70a74deca688addf8fa297ad61cedc2f49a5f
William <wtekpeh@murphygs.com>
2026-08-24 00:41:20 +0100

    Add length-prefixed message framing

--- producer/src/main.rs
+++ producer/src/main.rs
@@ -17,11 +17,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Message Five",
     ];
 
+    //Delimiter Framing
+    /*
     for message in messages {
         let framed_message = format!("{message}\n");
         stream.write_all(framed_message.as_bytes()).await?;
 
         println!("Producer sent: {message}");
+    }
+    */
+
+    //Fixed-Size Frames
+    for message in messages {
+        let payload = message.as_bytes();
+
+        let payload_length = payload.len() as u32;
+
+        let length_bytes = payload_length.to_be_bytes();
+
+        stream.write_all(&length_bytes).await?;
+        stream.write_all(payload).await?;
+
+        println!("Producer sent message of {payload_length} bytes: {message}");
     }
 
     Ok(())
